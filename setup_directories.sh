3a901679f1071b7235abe68f36243dd7db2abeb8
Junyi Mo <jm9847@nyu.edu>
2025-09-19 14:04:14 -0400

    Delete setup_directories.sh

--- setup_directories.sh
+++
@@ -1,8 +0,0 @@
-mkdir data/imputation_cache
-mkdir images-pdfs
-mkdir images-pdfs/appendix
-mkdir images-pdfs/section2
-mkdir images-pdfs/section3
-mkdir images-pdfs/section4
-mkdir images-pdfs/section5
-mkdir images-pdfs/section6
